a6ddff2b4259a3d152fe6e35f9573f419db68d8b
Eduardo Borsa <borsa.dado@gmail.com>
2021-02-02 18:05:44 -0300

    End of part 3
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- test/chatter_web/features/user_visits_homepage_test.exs
+++ test/chatter_web/features/user_visits_homepage_test.exs
@@ -8,10 +8,17 @@ defmodule ChatterWeb.Features.UserVisitsHomepageTest do
   end
 
   test "user visits rooms page to see a list of rooms", %{session: session} do
-    room = insert(:chat_room)
+    [room1, room2] = insert_pair(:chat_room)
 
     session
-    |> visit(Routes.chat_room_path(@endpoint, :index))
-    |> assert_has(Query.css(".room", text: room.name))
+    |> visit(rooms_index())
+    |> assert_has(room_name(room1))
+    |> assert_has(room_name(room2))
+  end
+
+  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+
+  defp room_name(room) do
+    Query.data("role", "room", text: room.name)
   end
 end
