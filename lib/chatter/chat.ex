c4a898c7fcb8dc226831d844d358252dd2a6c560
Eduardo Borsa <borsa.dado@gmail.com>
2021-02-03 06:30:56 -0300

    End of part 4
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- lib/chatter/chat.ex
+++ lib/chatter/chat.ex
@@ -1,7 +1,24 @@
 defmodule Chatter.Chat do
   alias Chatter.{Chat, Repo}
+  alias Chat.Room
 
   def all_rooms do
-    Chat.Room |> Repo.all()
+    Room |> Repo.all()
+  end
+
+  def find_room(id) do
+    Room
+    |> Repo.get!(id)
+  end
+
+  def new_chat_room() do
+    %Room{}
+    |> Chat.Room.changeset(%{})
+  end
+
+  def create_chat_room(params) do
+    %Room{}
+    |> Room.changeset(params)
+    |> Repo.insert()
   end
 end
